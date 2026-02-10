6a02e4d214e2740a324eb102638a369589e1946b
Riya-20070721 <riyardavda@gmail.com>
2026-02-10 23:08:22 +0530

    Create Practice1.py

---
+++ Practice1.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter Number 1:"))
+num2=int(input("Enter Number 2:"))
+num3=int(input("Enter Number 3:"))
+print(num1)
+print(num2)
+print(num3)
+Avg=(num1 + num2 + num3) / 3
+print("Average of three numbers:",Avg)
