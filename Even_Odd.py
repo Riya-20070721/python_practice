01e4e5899b7153ad1745d33a13863618aff7dc50
Riya-20070721 <riyardavda@gmail.com>
2026-02-17 15:07:55 +0530

    Create Even_Odd.py

---
+++ Even_Odd.py
@@ -0,0 +1,12 @@
+a = int(input("ENter your number 1:"))
+b = int(input("ENter your number 2:"))
+c = int(input("ENter your number 3:"))
+
+if (a>b and a>c):
+    print("a is maximum number")
+
+elif(b>a and b>c):
+    print("b is maximum number")
+
+else:
+    print("c is maximum number")
